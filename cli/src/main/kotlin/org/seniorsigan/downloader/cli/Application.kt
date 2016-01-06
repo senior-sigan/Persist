7ae012705a9574bb09829437a1e6877512093de5
Ilya Siganov <ilya.blan4@gmail.com>
2016-01-06 16:15:09 +0600

    fix cli build

--- cli/src/main/kotlin/org/seniorsigan/downloader/cli/Application.kt
+++ cli/src/main/kotlin/org/seniorsigan/downloader/cli/Application.kt
@@ -6,14 +6,8 @@ object Application {
     @JvmStatic
     fun main(args: Array<String>) {
         val service = Service()
-        var id: String
-        if (args.size == 0) {
-            //throw Exception("post id can't be empty")
-            id = "-58267631_18185"
-        } else {
-            id = args[0]
-        }
-        //service.saveAudios(id)
-        service.saveAudioToZip(id)
+        if (args.size == 0) throw Exception("post id can't be empty")
+        service.saveAudios(args[0])
+        //service.saveAudioToZip(args[0])
     }
 }
